34e96027eb60a69d75ce8e4624dfce267f6ec88e
Linda Njau <lindanjau21@gmail.com>
2024-04-26 16:23:26 +0300

    ExplorePage component

---
+++ src/ExplorePage.jsx
@@ -0,0 +1,11 @@
+import SearchUsers from "./SearchUsers";
+
+const ExplorePage = () => {
+    return (
+        <div>
+            <SearchUsers />
+        </div>
+    );
+};
+
+export default ExplorePage;
