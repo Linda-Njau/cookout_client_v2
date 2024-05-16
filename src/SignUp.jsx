608d77b72056fe57605fd323849d61edcdb337ed
Linda Njau <lindanjau21@gmail.com>
2024-05-16 15:27:08 +0300

    updated error assignment to dict from string matching

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -19,42 +19,29 @@ const SignUp = () => {
                 username: username,
             };
             const response = await postData('/users', payload);
-            console.log('Response:', response);
             localStorage.setItem('token', response.access_token);
             localStorage.setItem('user_id', response.user_id);
 
             window.dispatchEvent(new Event('storage'));
         } catch (error) {
-            console.log(`This is the Error: ${error}`)
-           if (error.response?.data?.error){
-            const errorData = error.response.data.error;
-            console.log(`This is the errorData: ${errorData}`);
-            const errorMessages = errorData.split(';').map(msg => msg.trim());
-            console.log(`This are the error messages: ${errorMessages}`);
+           if (error.response?.data?.errors){
+            const errors = error.response.data.errors;
            
            setPasswordError('');
            setUsernameError('');
            setEmailError('');
 
-           if (errorMessages.includes('Password must be at least 8 characters')) {
-            setPasswordError('Password must be at least 8 characters long');
-        } if (errorMessages.includes('please provide a password')) {
-            setPasswordError("Please provide a password")
-        } if (errorMessages.includes('Username already in use')) {
-            setUsernameError('Username already in use.');
-        } if (errorMessages.includes("Please provide a username")) {
-            setUsernameError("Please provide a username")
-        } if (errorMessages.includes('Please provide a valid email address')){
-            setEmailError("Please provide a valid email address")
-        } if (errorMessages.includes('Email address already in use')) {
-             setEmailError('Email already in use.');
-        } if (errorMessages.includes('Invalid email format')) {
-            setEmailError('Invalid email format');
-        }
-
-
+            if (errors.passwordError) {
+                setEmailError(errors.emailError);
+            }
+            if (errors.usernameError) {
+                setUsernameError(errors.usernameError);
+           }
+           if (errors.passwordError){
+                setPasswordError(errors.passwordError);
            }
         }
+    }
     };
     return (
         <div>
